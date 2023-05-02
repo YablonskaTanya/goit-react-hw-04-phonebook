ce510c14f043800b8ddf1e8a5a6c3469fc0a6ed5
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-05-02 16:46:03 +0300

    added reacr hooks

---
+++ src/components/ContactForm /ContactForm.jsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import css from './ContactForm.module.css';
+
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChangeName = e => {
+    const { value } = e.target;
+    setName(value);
+  };
+
+  const handleChangeNumber = e => {
+    const { value } = e.target;
+    setNumber(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onSubmit({ name: name, number: number });
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={css.form}>
+      <label className={css.formLabel}>
+        Name
+        <input
+          className={css.formInput}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeName}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+
+      <label className={css.formLabel}>
+        Number
+        <input
+          className={css.formInput}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChangeNumber}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+
+      <button className={css.addContactBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
